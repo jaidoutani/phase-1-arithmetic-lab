e9b4f19c4741d6e35870fa56940c3f79d3583997
Michael Bowlin <jaidoutani@gmail.com>
2022-02-02 15:23:06 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function multiply() {
-    return num1 * num2;
-}
+//is an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//generates a random integer greater than 0
+let random = Math.ceil(Math.random() * 10);
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 12;
+let num4 = 3;
+let mod = num3 / num4;
+
+//will return 20 as the highest number in the set
+let max = Math.max(1,5,8,20,10,16);
